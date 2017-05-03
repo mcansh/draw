aca492f02847789ab55e320c3e6617c47a66a1f0
Logan McAnsh <logan@mcan.sh>
2017-05-03 00:07:28 -0400

    fixed erase and color changing from not working
    erase is really just white
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -2,12 +2,14 @@ import { $, $$ } from './bling';
 
 const cssVars = window.getComputedStyle(document.documentElement);
 
-let activeColor = 'black';
 const canvas = $('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = activeColor;
+canvas.width = window.innerWidth + 1000;
+canvas.height = window.innerHeight + 1000;
+canvas.style.width = `${canvas.width}px`;
+canvas.style.height = `${canvas.height}px`;
+ctx.translate(0.5, 0.5);
+ctx.strokeStyle = cssVars.getPropertyValue('--black');
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 5;
@@ -16,19 +18,17 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 function pickColor() {
   const selectedColor = this.classList[1];
+  $('.active').classList.remove('erase');
+  $('.active').style.backgroundColor = cssVars.getPropertyValue(`--${selectedColor}`);
+  $('.active .border').style.borderColor = cssVars.getPropertyValue(`--${selectedColor}`);
+  ctx.strokeStyle = cssVars.getPropertyValue(`--${selectedColor}`);
+  console.log(selectedColor);
   if (selectedColor === 'erase') {
-    ctx.globalCompositeOperation = 'destination-out';
+    ctx.strokeStyle = 'white';
     ctx.lineWidth = 20;
-    $('.active').classList.add('erase');
-    $('.active').style.backgroundColor = '';
-    $('.active .border').style.borderColor = cssVars.getPropertyValue('--gray');
   } else {
-    $('.active').classList.remove('erase');
-    $('.active').style.backgroundColor = cssVars.getPropertyValue(`--${selectedColor}`);
-    $('.active .border').style.borderColor = cssVars.getPropertyValue(`--${selectedColor}`);
-    activeColor = $('.active').style.backgroundColor;
+    ctx.strokeStyle = cssVars.getPropertyValue(`--${selectedColor}`);
     ctx.lineWidth = 5;
-    ctx.strokeStyle = activeColor;
   }
 }
 
@@ -49,9 +49,8 @@ let lastY = 0;
 
 function draw(e) {
   if (!isDrawing) return;
-  // console.log(e);
   ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
+  ctx.moveTo(`${lastX}.5`, `${lastY}.5`);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
@@ -65,7 +64,7 @@ function downloadSVG() {
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
-  console.log(activeColor);
+  console.log(ctx.strokeStyle);
 });
 
 canvas.addEventListener('mousemove', draw);
